8d960a994ba83463939168c50fb7b42837e6ceb0
qesnault <quentin.esnault@lilo.org>
2020-05-28 21:48:21 +0200

    Ajout de la suppression d'une tache

--- todo/todoApp/urls.py
+++ todo/todoApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+# Gestion des différentes urls
 urlpatterns = [
     path('', views.todoListe, name='index_path'),
-    path('insertion', views.insertion, name='insertion_path')
+    path('insertion', views.insertion, name='insertion_path'),
+    path('suppression/<int:idTache>', views.suppression, name='suppression_path')
 ]
--- todo/todoApp/views.py
+++ todo/todoApp/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from .models import Tache
 
-# Create your views here.
+# Vu pour l'index
 def todoListe(request):
     context = {'Todolist' : Tache.objects.all() }
     return render(request, 'todoApp/Todolist.html', context)
 
+#Vu quand on insert une tache
 def insertion(request:HttpRequest):
     tache = Tache(nom = request.POST['nom'])
     tache.save()
+    return redirect('index_path')
+
+#Vu quand on supprime une tache
+def suppression(request, idTache):
+    tache = Tache.objects.get(id=idTache)
+    tache.delete()
     return redirect('index_path')
